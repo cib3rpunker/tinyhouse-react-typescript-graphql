3551577c9cb15cad65f90577209d32150ed98251
Viktor Óolen <ciberpunk3r@gmail.com>
2021-11-24 10:15:25 -0600

    📁4. Storing Data 📺4.7 - Processing Data from MongoDB

--- server/src/graphql/resolvers.ts
+++ server/src/graphql/resolvers.ts
@@ -1,24 +1,42 @@
 //import { IResolvers } from "apollo-server-express"
-import { IResolvers } from "@graphql-tools/utils";
-import { listings } from "../listings"
+import { IResolvers } from '@graphql-tools/utils';
+import { ObjectId } from 'mongodb';
+import { Database, Listing } from '../lib/types';
 
 export const resolvers: IResolvers = {
   //export const resolvers = {
   Query: {
-    listings: () => {
-      return listings;
-    }
+    listings: async (
+      _root: undefined,
+      _args: {},
+      { db }: { db: Database }
+    ): Promise<Listing[]> => {
+      return await db.listings.find({}).toArray();
+    },
   },
   Mutation: {
-    deleteListing: (_root: undefined, { id }: { id: string }) => {
-      for (let i = 0; i < listings.length; i++) {
-        if (listings[i].id === id) {
-          return listings.splice(i, 1)[0];
-        }
+    deleteListing: async (
+      _root: undefined,
+      { id }: { id: string },
+      { db }: { db: Database }
+    ): Promise<Listing> => {
+      const deleteRes = await db.listings.findOneAndDelete({
+        _id: new ObjectId(id),
+      });
 
-        throw new Error("failed to delete listing");
+      if (!deleteRes.value) {
+        throw new Error('failed to delete listing');
       }
-    }
-  }
-};
 
+      return deleteRes.value;
+    },
+  },
+  Listing: {
+    //* ApolloServer and many other GraghQL libs allow us to omit Simple or
+    //* Trivial Resolvers, so if a Resolver is not specified it willl read for the property of the object of the same name
+    //* Remember: even if don't specify Trivial Resolver, this fields are being resolve by the GraphQL Library implicitly
+    //title: (listing: Listing) => listing.title,
+    //image: (listing: Listing) => listing.image,
+    id: (listing: Listing): string => listing._id.toString(),
+  },
+};
